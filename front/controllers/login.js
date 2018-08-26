fe443ac65155976e2dbf836eb488813f476391f0
Ruben Mejia <rubennx7@gmail.com>
2018-08-26 18:19:46 -0500

    Solucionando bug de dashboardController
    Se soluciona bug de link de dashboardController y agrego documentacion al codigo

--- front/controllers/login.js
+++ front/controllers/login.js
@@ -1,16 +1,26 @@
+
+// Definimos una variable "app" y le definomos que VAMOS a user el modulo (app) <- app reucerden 
+//que es nombre de enuestra aplicacion que definimos en index.html
 var app = angular.module('app');
 
+//Aqui injectamos los componentes que usaremos $scope (obligatorio), loginResource(este seria un servicio que estariamos usando para peticiones)
+// y $location (ni idea esto xP)
 app.controller('loginController', function($scope, LoginResource, $location) {
+	//Esto seria la erstructura de una funcion en angularJs
+	//Esta funcion es llamada en views/login/index.html
 	$scope.login = function() {
 		console.log($scope.Login);
+		//Esto es una consulta
+		//fijense que NOMBRE_USUARIO Y CONTRASENA, son los nombre con los cuales ENVIARAN los datos al back
+		//Estos datos no los proporciona el ng-model accedemos con $scope 
 		LoginResource.query(
 			{
 				NOMBRE_USUARIO: $scope.Login.NOMBRE_USUARIO,
 				CONTRASENA: $scope.Login.CONTRASENA
 			},
-			function(response) {
+			function(response) { //Respuesta que nos trae el back
 				console.log(response[0].Login);
-
+				//hacemos los precosos que sean necesarios con la respuesta 
 				if (response[0].Login == 1) {
 					$location.path('/dashboard');
 				}
