50240aa4f7d5e001c2f31a9bc10aad7fe05b8b19
Ruben Mejia <rubennx7@gmail.com>
2018-08-26 17:20:57 -0500

    error corregido

--- front/scripts/router.js
+++ front/scripts/router.js
@@ -5,7 +5,7 @@ var app = angular.module('app', ['ngResource', 'ngRoute']);
 app.config(function($routeProvider) {
 	$routeProvider
 	.when('/', {
-		templateUrl : 'views/dashboard/index.html'
+		templateUrl : 'views/login/index.html'
 	})
 	.when('/login',{
 		templateUrl : 'views/login/index.html',
